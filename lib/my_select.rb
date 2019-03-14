12f1fd55f94025687995aa1e4782aae40484f718
Pete A <peter.ayresmusic@gmail.com>
2019-03-14 16:34:29 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,4 +1,5 @@
 def my_select(collection)
+<<<<<<< HEAD
    new_array = []
     i = 0
     while i < collection.length
@@ -8,4 +9,18 @@ def my_select(collection)
   i = i + 1
   end
   new_array
+=======
+  if block_given?
+   new_array = []
+    i = 0
+    while i < collection.length
+      new_array << if collection[i] == true
+      i = i + 1
+    end
+    new_array
+    end
+  else
+    puts "Hey! No block was given!"
+  end
+>>>>>>> 8aaf06bd6c38518db04d6c70a4475af9f6da8bd5
 end
